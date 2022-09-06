fe60115d94d9b57b7c8302bd828979ec481c45b5
SakethVangala11 <sakethvangala36323@gmail.com>
2022-09-05 22:52:30 -0400

    per function changed and commons.py

--- code/Commons.py
+++ code/Commons.py
@@ -1,6 +1,15 @@
 import math
 
+the={}
+
 #Return the 'p'-th thing from the sorted list 't'. 
-def per(self,t,p):
+def per(t,p):
     p=math.floor(((0.5 if p is None else p)*len(t))+0.5)
-    return t[max(1,min(len(t,p)))]
+    return t[max(1,min(len(t),p))]
+
+#converts t to string and print
+def oo(t):
+    str_t=str(t)
+    print(str_t)
+    return t
+
