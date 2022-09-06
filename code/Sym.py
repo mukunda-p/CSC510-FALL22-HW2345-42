89660af6b6a238d8ae2b637d6b0fc7ba262d7b3e
Mukunda Varma <varma13p@gmail.com>
2022-09-06 01:50:37 -0400

    Add div method in sym class

--- code/Sym.py
+++ code/Sym.py
@@ -1,8 +1,9 @@
+import math 
+
 class Sym:
     def __init__(self) -> None:
         self.n = 0
         self._has = dict()
-
 
     
     def add(self, v):
@@ -15,4 +16,11 @@ class Sym:
         return max(self._has, key=self._has.get)
     
 
-
+    def div(self):
+        fun = lambda p : p*math.log(p,2)
+        e = 0  
+        for key in self._has.keys():
+            if self._has[key] > 0 :
+                e = e - fun(self._has[key]/self.n)
+        
+        return e
