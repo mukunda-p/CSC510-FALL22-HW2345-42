7ce9f419989a16d01cc98ae77f349e4c9acf8c6e
PranaviSan <46625997+PranaviSan@users.noreply.github.com>
2022-09-12 19:16:46 -0400

    Added Row.py to the code

---
+++ code/Row.py
@@ -0,0 +1,26 @@
+import math 
+
+class Sym:
+    def __init__(self) -> None:
+        self.n = 0
+        self._has = dict()
+
+    
+    def add(self, v):
+        if v != '?':
+            self.n = self.n + 1
+            self._has[v] = 1 + self._has.get(v, 0)
+
+
+    def mid(self):
+        return max(self._has, key=self._has.get)
+    
+
+    def div(self):
+        fun = lambda p : p*math.log(p,2)
+        e = 0  
+        for key in self._has.keys():
+            if self._has[key] > 0 :
+                e = e - fun(self._has[key]/self.n)
+        
+        return e
