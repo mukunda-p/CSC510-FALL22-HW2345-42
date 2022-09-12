3ef428901de71437660f8369dea9b679cf74b128
srihithareddy-k <srihithareddykaalam@gmail.com>
2022-09-12 19:45:28 -0400

    Added column class

---
+++ code/Cols.py
@@ -0,0 +1,26 @@
+import Num
+import Sym
+class Cols:
+
+    def __init__(self,names):
+        self.names=names
+        self.all={}
+        self.klass=None
+        self.x={}
+        self.y={}
+
+        for column_name in self.names:
+            if column_name[0].isupper():
+                column=Num(names.index(column_name),column_name)
+            else
+                column=Sym(names.index(column_name),column_name)
+            
+            if column_name[-1]!=':':
+                if('!' in column_name or '+' in column_name or '-' in column_name):
+                    self.y.append(column)
+                else
+                    self.x.append(column)
+
+            if column_name[-1]=='!':
+                self.klass=column
+            self.all.append(column)
